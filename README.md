da312d39a9c2e869da920e8d4c1352d6e1ed726d
eljuanpepe <elpepechingon@outlook.com>
2025-12-22 19:21:05 -0600

    fixed

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-https://github.com/user-attachments/assets/0888754a-9dd8-438a-abf2-19d9ab8e0602
-
-it does not pause
+there's no pause button
--- badapple.js
+++ badapple.js
@@ -1,11 +1,29 @@
 const chessboard = document.getElementById("board-analysis-board")
-const playButton = document.querySelector('button[aria-label="Play / Pause"]')
-const pixels = ["bq", "bk", "br", "bn", "bb", "wp", "wb", "wn", "wr", "wk", "wq"]
+const boardSetup = document.getElementsByClassName("setup-board-component")[0]
 const audioUrl = chrome.runtime.getURL("BadApple.mp3")
+
 const framesPath = "frames/"
 const frameRate = 30
 const frameInterval = 1000 / frameRate
 const frameCount = 6572
+var buttonLabel = undefined
+const blackPixel = "bq"
+const whitePixel = "wk"
+
+function waitForElement(selector, callback) {
+    const observer = new MutationObserver(() => {
+        const element = document.querySelector(selector);
+        if (element) {
+            observer.disconnect();
+            callback(element);
+        }
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+}
 
 function clearBoard() {
     const pieces = chessboard.getElementsByClassName("piece")
@@ -15,7 +33,7 @@ function clearBoard() {
     }
 }
 
-function populateBoard() {
+function prepareBoard() {
     clearBoard()
     for (let i = 8; i >= 1; i--) {
         for (let j = 1; j <= 8; j++) {
@@ -39,69 +57,109 @@ function getFrameURL(index) {
 }
 
 function getImages() {
-    let index = 0
-    let urls = []
-    while (index++ < frameCount) {
-        urls.push(getFrameURL(index))
+    const urls = new Array(frameCount)
+
+    for (let i = 0; i < frameCount; i++) {
+        urls[i] = getFrameURL(i + 1)
     }
+
     return urls
 }
 
-async function getImageBrigtness(imageSrc) {
-    const img = document.createElement("img");
-    img.src = imageSrc;
-    img.style.display = "none";
+async function getImageBrigtness(imageSrc, canvas) {
+    const img = new Image();
     img.crossOrigin = "anonymous";
+    img.src = imageSrc;
 
-    document.body.appendChild(img);
-    var values = []
-
-    await new Promise(resolve => {
-        img.onload = resolve;
-    });
-
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
+    await img.decode()
 
     var ctx = canvas.getContext("2d");
     ctx.drawImage(img,0,0);
 
-    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
     var data = imageData.data;
-    var r,g,b;
+    const pixelCount = data.length / 4;
+    var values = new Array(pixelCount)
 
     for(var i = 0, len = data.length; i < len; i+=4) {
-        r = data[i];
-        g = data[i+1];
-        b = data[i+2];
+        const brightness = 0.2126*data[i] + 0.7152*data[i+1] + 0.0722*data[i+2];
 
-        let brigness = 0.2126*r + 0.7152*g + 0.0722*b
-        values.push(pixels[Math.floor(brigness/25)])
+        const pixelIndex = i / 4;
+        const brightnessThreshold = 100
+        values[pixelIndex] = brightness < brightnessThreshold ? blackPixel : whitePixel;
     }
+
     return values;
+}
+
+async function calculateFrames() {
+    let frames = new Array(frameCount)
+    const images = getImages()
+    var canvas = document.createElement("canvas");
+    canvas.width = 8;
+    canvas.height = 8;
+
+    for (let i = 0; i < images.length; i++) {
+        const image = images[i];
+        let frame = await getImageBrigtness(image, canvas)
+        frames[i] = frame
+        buttonLabel.innerText = `Calculating frames (${i}/${frameCount})`
+    }
+
+    return frames
 }
 
 let playing = false
 async function play() {
     if (playing) return
-    playing = true
 
+    playing = true
     const audio = new Audio(audioUrl)
-    const frames = getImages()
+    const frames = await calculateFrames()
+
+    buttonLabel.innerText = "Now playing bad apple"
+
+    prepareBoard()
+    await audio.play()
 
     let frameIndex = 0
-    populateBoard()
-    await audio.play()
-    let interval = setInterval(async () => {
-        frame = await getImageBrigtness(frames[frameIndex])
-        renderFrame(frame)
-        ++frameIndex
-        if (frameIndex >= frameCount) {
-            clearInterval(interval)
-            playing = false
+    let then = performance.now()
+    function step(now) {
+        const delta = now - then 
+
+        if (delta > frameInterval) {
+            const frame = frames[frameIndex]
+            renderFrame(frame)
+            ++frameIndex
+            then = now - (delta % frameInterval)
         }
-    }, frameInterval)
+
+        if (frameIndex < frameCount) {
+            requestAnimationFrame(step)
+        } else {
+            playing = false
+            buttonLabel.innerText = "Play bad apple"
+        }
+    }
+
+    requestAnimationFrame(step)
 }
 
-playButton.addEventListener("click", play)
+waitForElement(".setup-board-component", (el) => {
+    var buttonComponent = document.createElement('div')
+    buttonComponent.classList.add("setup-board-option-component", "sidebar-row-component", "setup-board-option-component")
+
+    var button = document.createElement("button")
+    buttonLabel = document.createElement("span")
+
+    buttonLabel.innerText = "Play bad apple"
+    buttonLabel.classList.add("setup-board-option-label")
+    button.append(buttonLabel)
+
+    buttonComponent.appendChild(button)
+
+    button.onclick = play
+
+    el.prepend(buttonComponent)
+})
+
